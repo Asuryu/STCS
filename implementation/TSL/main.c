56fba5a596aa25baf5bded6ddd352309dbffc505
Joaquim <joaquim_meruje@PT-PF3LQ31L.critical.pt>
2024-08-01 10:22:59 +0100

    Initial Declaration

--- implementation/TSL/main.c
+++ implementation/TSL/main.c
@@ -5,6 +5,27 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+typedef enum {false, true} bool;
+
+struct ThermalPair {
+    bool heater;
+    double thermistor;
+};
+
+struct HeaterGroup{
+    double thermistor1;
+    double thermistor2;
+    double thermistor3;
+    double thermistor4;
+};
+
+
+int setHeaterState(struct ThermalPair TP_block);
+int modify_temperatures(struct ThermalPair TP_block);
+int write_csv(struct ThermalPair TP_block);
+int verify_periods(int16_t clock);
+
+
 int main() {
 
     int fd;
@@ -15,9 +36,13 @@ int main() {
 
     fd = open(myfifo, O_CREAT);
 
-
     while(1){
         usleep(200 * 1000);
+        // check the last 8 bits from clock
+        // something to get the data from pive
+        // setHeaterState()
+        // modify_temperatures()
+        // write_csv()
         clock++;
         printf("%d",clock);
     }
