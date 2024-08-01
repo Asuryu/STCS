448f85e51befb053bb28c24c5b45f0cca20db95c
Joana Inês Ferrão <cswsis24-jf@criticalsoftware.com>
2024-08-01 16:38:00 +0100

    chore: implemented LIFO temperature history

--- implementation/TCF/tcf.c
+++ implementation/TCF/tcf.c
@@ -16,7 +16,7 @@
 float temps[HISTORY_SIZE][N_HEATERS];
 unsigned int enabled;
 
-
+//test function to be deleted
 void *controlTemp(void* pdata){
     while (enabled)
     {
@@ -27,15 +27,23 @@ void *controlTemp(void* pdata){
     
 }
 
+/* Set Temperatures Function
+ * Inserts new temperature array in the last position of the LIFO history array, or the first empty one if it exists.
+ * 
+ * Input/Params: 
+ * imutable float array of N_HEATERS size containing the values to insert
+ * 
+ * Output/Return: none
+ */
 void setTemps(const float newTemps[N_HEATERS]){
     unsigned i =0, h=0;
 
     while(i<HISTORY_SIZE)
-        if(isnan(temps[i++][0])) //encontrou um vazio
+        if(isnan(temps[i++][0])) //found empty spot on the history
             goto emptyAtI;
 
-    //não há "untapped"
-    //1. a fila vai para trás
+    // history is full:
+    // apply LIFO: pushes values back by 1
     i--;
     while(i > 0){
         h = 0;
@@ -46,7 +54,7 @@ void setTemps(const float newTemps[N_HEATERS]){
         i--;
     }
    
-   //adiciona o mais recente na ultima pos da fila
+   //adds the new temperatures to the last position of the list
     h = 0;
     while(h < N_HEATERS){
         temps[HISTORY_SIZE-1][h] = newTemps[h];
@@ -54,7 +62,7 @@ void setTemps(const float newTemps[N_HEATERS]){
     }
     return;
 
-//adiciona o mais recente no primeiro lugar disponivel
+//adds the new temperatures to the first empty position of the list
 emptyAtI: 
     h = 0;
     while(h < N_HEATERS){
@@ -63,7 +71,37 @@ emptyAtI:
     }
 }
 
-//try start thread
+/* Get Temperatures At (Index) Function
+ * Returns the temperatures at a given position of the history array - if there are any.
+ * 
+ * Input/Params: 
+ * integer containing the position wanted
+ * 
+ * Output/Return: 
+ *      NULL if the index is out of bounds or there is no data
+ *      imutable float array containing the temperatures at the given index
+ */
+const float *getTempsAt(int index){
+    return index < 0 || index >= HISTORY_SIZE || isnan(temps[index][0])  ?  NULL : temps[index];
+}
+
+
+/* Get Temperatures At (Index) Function
+ * Returns the temperature of a specific thermister at a given position of the history array - if there is any.
+ * 
+ * Input/Params: 
+ * integer containing the position of the history 
+ * integer containing the thermister position/id
+ * 
+ * Output/Return: 
+ *      NaN if any index is out of bounds or there is no data
+ *      float containing the temperature of the given thermister at the given history index
+ */
+const float getThermisterTempsAt(int hist_index, int therm_index){
+    return hist_index < 0 || therm_index < 0 || hist_index >= HISTORY_SIZE || therm_index >= N_HEATERS ? NAN : temps[hist_index][therm_index];
+}
+
+//try start thread (w/ enable set to 1)
 int enableTCF(pthread_t *pidThread) {
     enabled = 1;
     return pthread_create(pidThread,NULL, &controlTemp, NULL) != 0 ? -1 : 0;
@@ -80,13 +118,16 @@ int main(int argc, char **argv) {
     //init array with NaNs so later we can check if it has input temps or not
     while(i<HISTORY_SIZE)
         temps[i++][0] = NAN;
-
     
     if(enableTCF(&pidThread) == -1) return -1; 
+
+    //for testing only!
     printf("Press enter to end ");
     getchar();
     enabled = 0;
+    //- - - - - - - - -
     
+    //wait for thread to end to finish exec
     pthread_join(pidThread,NULL);
     return 0;
 }
