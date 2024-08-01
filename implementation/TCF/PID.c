f105ed3db694b6e2a0bcf958a80e0165ec36f778
Carolina Gonçalves <cswsis24-cg@criticalsoftware.com>
2024-08-01 15:50:47 +0100

    Fix: PID

--- implementation/TCF/PID.c
+++ implementation/TCF/PID.c
@@ -1,24 +1,33 @@
 #include "PID.h"
 
 #include <stdlib.h>
+#include <stdio.h>
 
 int PID(int goal_temperature, int freq, int *current_temperature, int *state) {
     int temperature_size = sizeof(current_temperature) / sizeof(current_temperature[0]);
     float *error = (float *)malloc(temperature_size * sizeof(float));
-    float *goal_temperature_array = (float *)malloc(temperature_size * sizeof(float));
+
+    int final_state;
 
     float Kp = 3.0; 
-    float Ki = 1.0; 
-    float Kd = 0.05;
+    float Ki = 0.005; 
+    float Kd = 0;
+    //float Kd = 0.05;
+
+    printf("Data:\nFreq: %d \t Kp: %f ; Ki: %f ; Kd: %f\nErrors: ", freq, Kp, Ki, Kd );
 
     for(int i = 0; i < temperature_size; i++) {
-        goal_temperature_array[i] = goal_temperature;
+        error[i] = goal_temperature - current_temperature[i];
+        printf("%f \t", error[i]);
     }
+    
+    printf("\n\n[1] %f\n[2] %f\n[3] %f\n" , state[0] + error[2] * (Kp + Ki * freq / 2 + 2 * Kd / freq),  error[1] * (Ki * freq - 4 * Kd / freq), error[0] * (Kd * 2 / freq + Ki * freq / 2 - Kp));
 
-        error[i] = goal_temperature_array[i] - current_temperature[i];
-        state[i] = state[i - 1] + error[i] * (Kp + Ki * freq / 2 + 2 * Kd / freq) +
-                    error[i - 1] * (-Kp + Ki * freq / 2 - 4 * Kd / freq) +
-                    error[i - 2] * Kd / freq;
+    final_state = state[sizeof(current_temperature) / sizeof(current_temperature[0])-2] + error[2] * (Kp + Ki * (freq / 2) + 2 * (Kd / freq)) 
+                    + error[1] * (Ki * freq - 4 * (Kd / freq))
+                    + error[0] * (Kd * (2 / freq) + Ki * (freq / 2) - Kp);
+    
+     free(error);
 
-    return state[temperature_size - 1];
+    return final_state;
 }
--- implementation/TCF/main.c
+++ implementation/TCF/main.c
@@ -1,18 +1,44 @@
 #include <stdio.h>
 
 #include "PID.h"
+#include <stdlib.h>
 
 int main() {
 
-    int h=5;
-    int num_interacoes = 10;
-    int Tref=-5;
-    int i;
-    int temp [10] = {0,1,2,3,4,5,6,7,8,9};
-    int estadoanterior [10] = {1,0,1,0,1,1,1,1,1,1};
+    int freq = 5;
+    int goal_temperature = 10; 
+    int size = 1000; 
+    int current_temperature[size];
+    current_temperature[0]=1;
+    current_temperature[1]=2;
+    current_temperature[2]=3;
+    int state[3] = {0, 0, 0};
 
-    int v = PID(Tref, h, temp, estadoanterior);
-    printf("v = %d\n", v);
+    int final_state = PID(goal_temperature, freq, current_temperature, state);
+    printf("Final state: %d\n", final_state);
+
+    for (int j=3;j<size;j++){
+
+    final_state = PID(goal_temperature, freq, current_temperature, state);
+    printf("Final state: %d\n", final_state);
+
+    if (final_state>0){
+        current_temperature[j]= current_temperature[j-1]+2;}
+    if (final_state<0){
+        current_temperature[j]= current_temperature[j-1]-2;}
+    }
+
+    FILE *file = fopen("temperaturas.txt", "w");
+    if (file == NULL) {
+        printf("Erro ao abrir o arquivo!\n");
+        return 1;
+    }
+
+    for (int j = 0; j < size; j++) {
+        fprintf(file, "%d\n", current_temperature[j]);
+    }
+
+    fclose(file);
 
     return 0;
 }
