bfacb82d3ec67f72947bc34a597bb81bb9041fab
Joana Inês Ferrão <cswsis24-jf@criticalsoftware.com>
2024-08-01 16:23:07 +0100

    chore: implemented LIFO temperature history

--- implementation/TCF/tcf.c
+++ implementation/TCF/tcf.c
@@ -4,6 +4,8 @@
 #include <pthread.h>
 #include<unistd.h>
 
+#include <ctype.h>
+
 #define N_HEATERS 4
 #define HISTORY_SIZE 2
 
@@ -19,31 +21,72 @@ void *controlTemp(void* pdata){
     while (enabled)
     {
         printf(".");
-        fflush(stdout);
         sleep(1);
     }
     return 0;
     
 }
 
+void setTemps(const float newTemps[N_HEATERS]){
+    unsigned i =0, h=0;
+
+    while(i<HISTORY_SIZE)
+        if(isnan(temps[i++][0])) //encontrou um vazio
+            goto emptyAtI;
+
+    //não há "untapped"
+    //1. a fila vai para trás
+    i--;
+    while(i > 0){
+        h = 0;
+        while(h < N_HEATERS){
+            temps[i-1][h] = temps[i][h];
+            h++;
+        }
+        i--;
+    }
+   
+   //adiciona o mais recente na ultima pos da fila
+    h = 0;
+    while(h < N_HEATERS){
+        temps[HISTORY_SIZE-1][h] = newTemps[h];
+        h++;
+    }
+    return;
+
+//adiciona o mais recente no primeiro lugar disponivel
+emptyAtI: 
+    h = 0;
+    while(h < N_HEATERS){
+        temps[i-1][h] = newTemps[h];
+        h++;
+    }
+}
+
+//try start thread
 int enableTCF(pthread_t *pidThread) {
+    enabled = 1;
     return pthread_create(pidThread,NULL, &controlTemp, NULL) != 0 ? -1 : 0;
 }
 
 
 
 int main(int argc, char **argv) {
-    //memset(&temps, nan, sizeof(temps)); //init array with NaNs so later we can check if it has input temps or not
-    enabled = 1;
+    setbuf(stdout, NULL); 
+   
     pthread_t pidThread;
+    unsigned i=0;
+    
+    //init array with NaNs so later we can check if it has input temps or not
+    while(i<HISTORY_SIZE)
+        temps[i++][0] = NAN;
+
     
     if(enableTCF(&pidThread) == -1) return -1; 
-
     printf("Press enter to end ");
     getchar();
     enabled = 0;
     
-
     pthread_join(pidThread,NULL);
     return 0;
 }
