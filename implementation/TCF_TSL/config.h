61df870a325e14cc43585223f98abab348d2fd53
Tomás Silva <tomas.silva@criticalsoftware.com>
2024-09-06 15:48:59 +0100

    Updated presentation for neurodiversity program and moved config file to root

--- implementation/TCF_TSL/config.h
+++
@@ -1,8 +0,0 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-
-// Define response informations for pipe
-#define TEMP_INFO_PIPE "/tmp/temp_info_pipe"
-#define RESPONSE_PIPE "/tmp/response_pipe"
-
-#endif // !CONFIG_H
